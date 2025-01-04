6237db8b500930b8523a5293f497a51ed0798bcf
Hamitha_KD <hamithakd99@gmail.com>
2025-01-04 13:32:12 +0530

    make category,subcategory filter

--- frontend/src/Pages/Collection.jsx
+++ frontend/src/Pages/Collection.jsx
@@ -27,9 +27,23 @@ const Collection = () => {
     }
   }
 
+  const applyFilter = () => {
+    let productsCopy = products.slice();
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+
+    }
+    
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
+    }
+    setfilterProducts(productsCopy);
+  }
+
   useEffect(()=> {
-    setfilterProducts(products)
-  },[])
+    applyFilter();
+
+  },[category, subCategory])
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
