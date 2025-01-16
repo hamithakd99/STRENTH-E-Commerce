c83904d8a1490cb2f88d390dc995372f2a645f49
Hamitha_KD <hamithakd99@gmail.com>
2025-01-16 21:44:31 +0530

    create description section

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -67,6 +67,25 @@ const Product = () => {
               ))}
             </div>
           </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5'/>
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original Product.</p>
+            <p>Cash on Delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* --------------- Description ------------------- */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, nostrum! Voluptatibus eos tenetur debitis. Officiis atque quo dolores iusto, quas fugit modi non. Itaque fugiat distinctio ea, doloribus et amet provident. Provident repellendus minima quasi perspiciatis mollitia tempore consectetur suscipit similique eos. Earum pariatur illo ullam repudiandae animi magni nihil itaque, accusantium, eum odit accusamus.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem animi labore corrupti, similique itaque magnam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptatibus vel ea!</p>
         </div>
       </div>
     </div>
